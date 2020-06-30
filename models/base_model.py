fa3d1d3035cca5b8bdaac364d3790990a70cf097
kimha1030 <1478@holbertonschool.com>
2020-06-30 15:07:09 -0500

    Update BaseModel Class

--- models/base_model.py
+++ models/base_model.py
@@ -12,23 +12,22 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """ [Constructor of class]
         """
-        if kwargs or len(kwargs) > 0:
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "id":
                     self.id = v
-                elif k == "create_at":
+                elif k == "created_at":
                     self.__dict__[k] =\
                      datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                elif k == "update_at":
+                elif k == "updated_at":
                     self.__dict__[k] =\
                      datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    if k != "__class__":
+                elif k != "__class__":
                         setattr(self, k, v)
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             models.storage.new(self)
 
     def __str__(self):
@@ -42,7 +41,7 @@ class BaseModel():
         """ This method updates "updated_at"
             with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
@@ -50,7 +49,7 @@ class BaseModel():
             contains the class attributes
         """
         new_dict = self.__dict__.copy()
-        new_dict['__class__'] = type(self).__name__
+        new_dict['__class__'] = self.__class__.__name__
         for k, v in new_dict.items():
             if isinstance(v, datetime):
                 new_dict[k] = v.strftime("%Y-%m-%dT%H:%M:%S.%f")
