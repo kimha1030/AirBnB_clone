e19d99e4fe33fd38be37001c268b367c25df7ccb
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-07-01 16:22:27 +0000

    add unittest review and place class

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""unittest User class
+"""unittest Amenity class
 """
 import unittest
 from models.amenity import Amenity
@@ -7,15 +7,15 @@ import pep8
 
 
 class TestState(unittest.TestCase):
-    """Test User class"""
+    """Test Amenity class"""
 
-    def test_init_pep8_conformance(self):
+    def test_amenity_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['./tests/test_models/test_amenity.py'])
         self.assertEqual(result.total_errors, 0)
 
-    def test_init_docstring(self):
+    def test_amenity_docstring(self):
         """Check the docstring in the class"""
         self.assertTrue(len(Amenity.__doc__) >= 1)
 
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,13 +9,13 @@ import pep8
 class TestUser(unittest.TestCase):
     """Test User class"""
 
-    def test_init_pep8_conformance(self):
+    def test_User_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['./tests/test_models/test_user.py'])
         self.assertEqual(result.total_errors, 0)
 
-    def test_init_docstring(self):
+    def test_User_docstring(self):
         """Check the docstring in the class"""
         self.assertTrue(len(User.__doc__) >= 1)
 
